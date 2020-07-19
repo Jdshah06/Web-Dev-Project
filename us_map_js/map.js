e6823fc868ec809bdb9ef83d95ee7b950e535c73
Meera Shah <meera.shah96@gmail.com>
2020-07-19 11:48:17 -0700

    fixed map colors

--- us_map_js/map.js
+++ us_map_js/map.js
@@ -1,5 +1,5 @@
 var accessToken = API_KEY;
-const map = L.map('map').setView([37.8, -96], 4);
+var map = L.map('map').setView([37.8, -96], 4);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
     id: 'mapbox/light-v9',
@@ -15,19 +15,64 @@ L.geoJson(statesData).addTo(map);
 // add color function: red = #8B0000, blue = #0000FF, hover = #D3D3D3
 
 // *TODO: fill data here 
+// var pctData = "REP.json"
+// var pctjson; 
+// var pct = [];
 
-// function getColor(candidate) {
-//     return '#8B0000'
+// // grab data
+// // console.log(pctData);
+
+
+// console.log(pct)
+
+
+// function getColor() {
+//     d3.json(pctData).then(function(data){
+//         data.forEach(i => pct.push(i[3]))
+//         console.log(pct)
+//         for (var d = 0; d < pct.length; d++) {
+//             if (d >= 45) {
+//                 console.log("#8B0000")
+//                 return "#8B0000"
+//             } else if (d >= 40) {
+//                 console.log("#F5F5DC")
+//                 return "#F5F5DC"
+//             } else if (d < 40) {
+//                 console.log("#0000FF")
+//                 return "#0000FF"
+//             }
+//         }
+//     })
 // }
+
+function getColor(d){
+    return d > 50 ? "#8B0000":
+                    "#0000FF"
+}
+
+
+
+// function getColor(array) {
+//     for (var d = 0; d < pct.length; d++) {
+//         if (d > 50) {
+//             console.log("#8B0000")
+//         } else {
+//             console.log("#0000FF")
+//         }
+//     }
+// }
+
+// getColor(pct)
 
 function style(feature) {
     return {
-        fillColor: '#8B0000',
+        fillColor: getColor(feature.properties.density),
         weight: 2,
         opacity: 1,
         color: 'white',
         dashArray: '1',
         fillOpacity: 1
+        
     };
 }
 
@@ -84,5 +129,7 @@ L.geoJson(statesData, {
     style: style,
     onEachFeature: hover
 }).addTo(map);
+
+console.log(statesData)
 
 
